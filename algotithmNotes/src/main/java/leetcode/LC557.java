bdf6f6250577527433a37a8e88626c779762eed1
agjycxys <15829350485@163.com>
2021-06-03 22:09:11 +0800

    反转字符串中的单词 III

--- algotithmNotes/src/main/java/leetcode/LC557.java
+++ algotithmNotes/src/main/java/leetcode/LC557.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Scanner;
+
 /**
  * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
  */
@@ -7,11 +9,25 @@ public class LC557 {
 
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            System.out.println(reverseWords(scanner.nextLine()));
+        }
     }
 
 
-    public String reverseWords(String s) {
-
+    public static String reverseWords(String s) {
+        String[] strAfterSplit = s.split(" ");
+        String reverseWords = "";
+        for (int i = 0; i < strAfterSplit.length; i++) {
+            String str = strAfterSplit[i];
+            String reverseStr = "";
+            for (int j = str.length() - 1; j >= 0; j--) {
+                char c = str.charAt(j);
+                reverseStr += c;
+            }
+            reverseWords += reverseStr + " ";
+        }
+        return reverseWords.trim();
     }
 }
